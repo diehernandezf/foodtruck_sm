618690559baca078307fb2e798ca2afd0c2f11d7
diegovntonio <diegoh200312@gmail.com>
2025-10-28 15:33:48 -0300

    proyecto factorizado

--- apps/pagos/apps.py
+++ apps/pagos/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class PagosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pagos'
+    name = 'apps.pagos'
     verbose_name = 'Pagos'
--- apps/usuarios/apps.py
+++ apps/usuarios/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class UsuariosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usuarios'
+    name = 'apps.usuarios'
     verbose_name = 'Usuarios'
